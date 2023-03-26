e2de91d75acbc69928938b48c934576f568c4177
Serega Noreehn <128985049+nxreehn@users.noreply.github.com>
2023-03-26 22:39:29 +0600

    Update blmlx.js

--- blmlx.js
+++ blmlx.js
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @name         BilimalXtnd.
+// @namespace    noreehn.
+// @version      1.0
+// @description  Данный скрипт возвращает возможность выставить оценки в системе билимал даже после того как база была закрыта.
+// @author       noreehn.
+// @match        https://school.bilimal.kz/*
+// @icon         https://cdn-icons-png.flaticon.com/512/258/258304.png
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_setClipboard
+// ==/UserScript==
 (function() {
     'use strict';
     console.log("Script Loaded.");
